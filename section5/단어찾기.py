7d28d1a77476b03badbde47737a6f51237689099
onlydev7777 <onlydev7777@gmail.com>
2024-07-31 15:44:56 +0900

    section5 commit 3

--- section5/단어찾기.py
+++ section5/단어찾기.py
@@ -1,23 +1,41 @@
 import sys
-from collections import deque
 
 sys.stdin = open("단어찾기.txt", "rt")
 
 N = int(input())
-arr = []
-deq2 = deque()
+
+# -- 2개의 리스트를 사용해서 풀기 --
+# arr = []
+# deq2 = deque()
+#
+# for _ in range(N):
+#   arr.append(input())
+#
+# for _ in range(N - 1):
+#   deq2.append(input())
+#
+# deq = deque(arr)
+#
+# for v in arr:
+#   if v in deq2:
+#     deq.remove(v)
+#     deq2.remove(v)
+#
+# print(deq[0])
+
+# --dictionary 사용해서 풀기--
+
+d = dict()
 
 for _ in range(N):
-  arr.append(input())
+  word = input()
+  d[word] = d.get(word, 0) + 1
 
 for _ in range(N - 1):
-  deq2.append(input())
+  word = input()
+  d[word] = d.get(word, 0) - 1
 
-deq = deque(arr)
-
-for v in arr:
-  if v in deq2:
-    deq.remove(v)
-    deq2.remove(v)
-
-print(deq[0])
+for key, val in d.items():
+  if val == 1:
+    print(key)
+    break
