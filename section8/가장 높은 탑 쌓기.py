57795f5c31fabfa59cff0a987f75f5be85a03ae8
onlydev7777 <onlydev7777@gmail.com>
2024-09-02 09:30:57 +0900

    section8 commit 3

--- section8/가장 높은 탑 쌓기.py
+++ section8/가장 높은 탑 쌓기.py
@@ -4,15 +4,11 @@ sys.stdin = open("가장 높은 탑 쌓기.txt", "rt")
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
+arr.sort(key=lambda x: x[0], reverse=True)
 
 dy = [0] * N
 dy[0] = arr[0][1]
 res = arr[0][1]
-
-arr.sort(key=lambda x: x[0], reverse=True)
-for v in arr:
-  print(v)
-  
 for i in range(1, N):
   maxx = 0
   for j in range(i - 1, -1, -1):
@@ -24,4 +20,3 @@ for i in range(1, N):
     res = dy[i]
 
 print(res)
-print(dy)
